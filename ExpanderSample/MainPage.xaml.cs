22f6b87dd4b2996540b4cb50404d0080223a4860
sachin <sachinkanadia@hotmail.com>
2020-10-09 18:25:30 +0100

    Initial check-in of module ExpanderSample

---
+++ ExpanderSample/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ExpanderSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void Expander_PropertyChanged(System.Object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == "State")
+            {
+                var state = ((Expander)sender).State;
+            }
+        }
+
+        void Expander_PropertyChanging(System.Object sender, Xamarin.Forms.PropertyChangingEventArgs e)
+        {
+            if (e.PropertyName == "State")
+            {
+                var state = ((Expander)sender).State;
+            }
+        }
+    }
+}
