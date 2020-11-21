2d26865ccc3fe51003d66f386044b3a881544760
sachin <sachinkanadia@hotmail.com>
2020-11-21 23:13:27 +0000

    Built sample

--- ExpanderSample/MainPage.xaml.cs
+++ ExpanderSample/MainPage.xaml.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.ObjectModel;
 using Xamarin.Forms;
-
 namespace ExpanderSample
 {
     public partial class MainPage : ContentPage
@@ -13,22 +7,15 @@ namespace ExpanderSample
         public MainPage()
         {
             InitializeComponent();
+
+            Items.Add("Apple");
+            Items.Add("Pear");
+            Items.Add("Banana");
+            Items.Add("Orange");
+            Items.Add("Pineapple");
+            Items.Add("Grapes");
         }
 
-        void Expander_PropertyChanged(System.Object sender, System.ComponentModel.PropertyChangedEventArgs e)
-        {
-            if (e.PropertyName == "State")
-            {
-                var state = ((Expander)sender).State;
-            }
-        }
-
-        void Expander_PropertyChanging(System.Object sender, Xamarin.Forms.PropertyChangingEventArgs e)
-        {
-            if (e.PropertyName == "State")
-            {
-                var state = ((Expander)sender).State;
-            }
-        }
+        public ObservableCollection<string> Items { get; private set; } = new ObservableCollection<string>();
     }
 }
